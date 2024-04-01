89e0ba8ce7066c3dbb8cbfff20dbbe687b6cc165
Timothy Small <smalls.online@icloud.com>
2024-04-01 12:47:43 -0400

    Add alternative argument for CLI switches

--- src/ConsoleApp/Exceptions/MissingConfigurationValueException.cs
+++ src/ConsoleApp/Exceptions/MissingConfigurationValueException.cs
@@ -6,7 +6,15 @@ public sealed class MissingConfigurationValueException : Exception
     {
     }
 
+    public MissingConfigurationValueException(string key, string commandLineSwitch) : base($"Configuration value for key '{key}' (or '{commandLineSwitch}') is missing.")
+    {
+    }
+
     public MissingConfigurationValueException(string key, Exception innerException) : base($"Configuration value for key '{key}' is missing.", innerException)
+    {
+    }
+
+    public MissingConfigurationValueException(string key, string commandLineSwitch, Exception innerException) : base($"Configuration value for key '{key}' (or '{commandLineSwitch}') is missing.", innerException)
     {
     }
 }
