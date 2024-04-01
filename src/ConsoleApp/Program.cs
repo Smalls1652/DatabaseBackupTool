b8df7f37fd907b917657fd92074ae79e8fbd4ead
Timothy Small <smalls.online@icloud.com>
2024-04-01 12:21:15 -0400

    Add logic for setting up database providers at startup

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using DatabaseBackupTool.ConsoleApp;
+﻿using DatabaseBackupTool.ConsoleApp;
+using DatabaseBackupTool.ConsoleApp.DatabaseProviders;
 using DatabaseBackupTool.ConsoleApp.Extensions;
 using DatabaseBackupTool.ConsoleApp.Models;
 using DatabaseBackupTool.ConsoleApp.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -44,15 +46,32 @@ if (builder.Configuration.GetValue<bool>("USE_KEY_VAULT"))
     kvService.Dispose();
 }
 
-builder.Services
-    .AddMainService(
-        options =>
+string? databaseTypeSettingValue = builder.Configuration.GetValue<string>("DATABASE_TYPE");
+DatabaseType databaseType = databaseTypeSettingValue is not null
+    ? Enum.Parse<DatabaseType>(databaseTypeSettingValue, ignoreCase: true)
+    : throw new NullReferenceException("DATABASE_TYPE or --database-type is required");
+
+if (databaseType == DatabaseType.Postgres)
+{
+    builder.Services
+        .AddPostgresDatabaseProvider(options =>
         {
             options.Host = builder.Configuration.GetValue<string>("DATABASE_HOST") ?? throw new NullReferenceException("DATABASE_HOST or --host is required");
             options.Port = builder.Configuration.GetValue<int>("DATABASE_PORT") == 0 ? 5432 : builder.Configuration.GetValue<int>("DATABASE_PORT");
             options.Username = builder.Configuration.GetValue<string>("DATABASE_USERNAME") ?? throw new NullReferenceException("DATABASE_USERNAME or --username is required");
             options.Password = builder.Configuration.GetValue<string>("DATABASE_PASSWORD") ?? throw new NullReferenceException("DATABASE_PASSWORD or --password is required");
             options.Database = builder.Configuration.GetValue<string>("DATABASE_NAME") ?? throw new NullReferenceException("DATABASE_NAME or --database is required");
+        });
+}
+else
+{
+    throw new Exception("An error occurred while selecting the database provider.");
+}
+
+builder.Services
+    .AddMainService(
+        options =>
+        {
             options.OutputPath = builder.Configuration.GetValue<string>("OUTPUT_PATH") ?? throw new NullReferenceException("OUTPUT_PATH or --output-path is required");
 
             string? backupLocationSettingValue = builder.Configuration.GetValue<string>("BACKUP_LOCATION");
