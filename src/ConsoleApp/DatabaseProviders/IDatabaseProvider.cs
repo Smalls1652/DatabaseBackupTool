8c1ced8137e43b29211e25065967772cdd1e92c6
Timothy Small <smalls.online@icloud.com>
2024-04-01 12:18:43 -0400

    Add code docs for IDatabaseProvider

--- src/ConsoleApp/DatabaseProviders/IDatabaseProvider.cs
+++ src/ConsoleApp/DatabaseProviders/IDatabaseProvider.cs
@@ -1,8 +1,23 @@
 namespace DatabaseBackupTool.ConsoleApp.DatabaseProviders;
 
+/// <summary>
+/// Interface for database providers.
+/// </summary>
 public interface IDatabaseProvider
 {
+    /// <summary>
+    /// Dump the database to the specified output path.
+    /// </summary>
+    /// <param name="outputPath">The path to dump the database to.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
     Task DumpDatabaseAsync(string outputPath, CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Compress the dump at the specified output path.
+    /// </summary>
+    /// <param name="outputPath">The path to the dumped database.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The path to the compressed dump.</returns>
     Task<string> CompressDumpAsync(string outputPath, CancellationToken cancellationToken = default);
 }
